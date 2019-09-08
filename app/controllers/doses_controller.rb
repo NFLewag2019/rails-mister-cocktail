08e4d625f909596e46c0a571f28887183485f9b7
Neal Franc <nealfranc@gmail.com>
2019-09-08 15:41:30 +0300

    Mr. Cocktail WIP

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,16 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(description: params[:dose][:description])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.ingredient = @ingredient
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @dose.save
+    redirect_to cocktail_path(@cocktail)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  # get 'index', to: 'cocktails#index', as: :index
+  root to: "cocktails#index"
+  resources :cocktails, only: [:show, :new, :create] do
+    resources :doses, only: [:create, :new]
+  end
+  # get 'show', to: 'cocktails#show', as: :show
+  # get 'new', to: 'cocktails#new', as: :new
+  # get 'create', to: 'cocktails#create', as: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
